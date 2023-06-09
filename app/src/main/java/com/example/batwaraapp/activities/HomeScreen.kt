dee043c6494605e382baf7aafa55b2e169eb5441
Vinayak <vinayakfredrics@gmail.com>
2023-06-09 15:41:38 +0530

    Changes: UI updations.

--- app/src/main/java/com/example/batwaraapp/activities/HomeScreen.kt
+++ app/src/main/java/com/example/batwaraapp/activities/HomeScreen.kt
@@ -3,8 +3,11 @@ package com.example.batwaraapp.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.batwaraapp.R
 import com.example.batwaraapp.databinding.ActivityHomeScreenBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeScreen : AppCompatActivity() {
 
@@ -13,6 +16,14 @@ class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home_screen)
+        supportActionBar?.hide()
+        /**
+         * Bottom Navigation setup.
+         * Back actions will be handled automatically via below code.
+         * */
+        val navController = findNavController(R.id.nav_host_fragment)
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        navView.setupWithNavController(navController)
 
     }
 
